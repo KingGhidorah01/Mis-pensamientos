39858a739d7795a947ce805b7d301f4544de2237
KingGhidorah01 <martin.andrade@alumnos.udg.mx>
2021-02-18 15:16:46 -0600

    App de pensamientos con vue y laravel 5.8

---
+++ resources/js/components/FormComponent.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="card">
+        <div class="card-header">¿En que estás pensando ahora?</div>
+
+        <div class="card-body">
+            <form action="" v-on:submit.prevent="newThought()">
+                <div class="form-group">
+                    <label>Ahora estoy pensando en:</label>
+                    <input type="text" class="form-control" name="thought" v-model="description">
+                </div>
+                <button type="submit" class="btn btn-primary">Enviar pensamiento</button>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            description: ''
+        }
+    },
+    mounted() {
+        console.log('Component mounted.')
+    },
+    methods: {
+        newThought() {
+            const params = {
+                description: this.description
+            };
+            this.description = '';
+            axios.post('/thoughts', params).then((response) => {
+                const thought = response.data;
+                this.$emit('new', thought);
+            });
+        }
+    }
+}
+</script>
